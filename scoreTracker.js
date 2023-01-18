80b6c0ab1189176a6fa35dbf08677cf7fecef333
Sneha Trivedi <snehatrivedi1009@gmail.com>
2023-01-18 16:55:59 +0530

    Feat: create a new test in the file

--- scoreTracker.js
+++ scoreTracker.js
@@ -1,27 +1,20 @@
 const rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
-
+const roll=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
 function getBowlingScore(rolls) {
-	//let frame=1;
-	let score=0;
-	//let checkFrame=1;
-	if (rolls.length < 20) {
-		return -1;
-
-	}
-
-	for (let index = 0; index < rolls.length; index++) {
-		//if(checkFrame==1){
-		if((rolls[index]+rolls[index+1])<10){
-			score+=rolls[index]+rolls[index+1];
-			index+=1;
-		}
-
-		
-	}
-	return score;
-
-	
+    let score=0;
+ 
+    for (let index = 0; index < rolls.length-1; index++) {
+        
+        if((rolls[index]+rolls[index+1])<10){
+            score+=rolls[index]+rolls[index+1];
+            index=index+1;
+        }
+       
+    }
+    return score;
 
 }
 console.log(getBowlingScore(rolls));
+console.log(getBowlingScore(roll));
+
 module.exports = { getBowlingScore };
--- scoreTracker.test.js
+++ scoreTracker.test.js
@@ -2,11 +2,11 @@
 const { getBowlingScore } = require('./scoreTracker');
 describe('Calculate Bowling score',()=>
 {
-	describe('Frame',()=>{
-		it('case 1',()=>
-		{
-			expect(getBowlingScore()).toBe(90);
-		});
+    describe('Frame',()=>{
+        it('case 1',()=>
+        {
+            expect(getBowlingScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+        });
     
-	});
+    });
 });
