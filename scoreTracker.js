2cf2524cee97c1a46f7c583994f1eb71b527ac2a
Sneha Trivedi <snehatrivedi1009@gmail.com>
2023-01-18 17:05:33 +0530

    Feat: create a new test in the file

--- scoreTracker.js
+++ scoreTracker.js
@@ -1,17 +1,17 @@
 const rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
 const roll=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
 function getBowlingScore(rolls) {
-    let score=0;
+	let score=0;
  
-    for (let index = 0; index < rolls.length-1; index++) {
+	for (let index = 0; index < rolls.length-1; index++) {
         
-        if((rolls[index]+rolls[index+1])<10){
-            score+=rolls[index]+rolls[index+1];
-            index=index+1;
-        }
+		if((rolls[index]+rolls[index+1])<10){
+			score+=rolls[index]+rolls[index+1];
+			index=index+1;
+		}
        
-    }
-    return score;
+	}
+	return score;
 
 }
 console.log(getBowlingScore(rolls));
